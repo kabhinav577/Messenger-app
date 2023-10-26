3c2778ba71ca8476b5ad2572e2fcd0288e9a3474
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-10-26 19:58:39 +0530

    create getUsers action and call in users layout

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
 import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     <Sidebar>
       <div className="h-full">{children}</div>
