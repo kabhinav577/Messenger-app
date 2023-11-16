605695f0553f928abd3aea2b55b2fb6ab204e42a
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2023-11-16 10:41:39 +0530

    Creating useActiveList and useActiveChannel hooks for checking how many user is online

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -11,5 +11,11 @@ export const pusherServer = new PusherServer({
 
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  { cluster: 'ap2' }
+  {
+    channelAuthorization: {
+      endpoint: '/api/puhser/auth',
+      transport: 'ajax',
+    },
+    cluster: 'ap2',
+  }
 );
